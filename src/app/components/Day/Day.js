1ecfe250300f813c44ad30a58073a94cb1ccc5c3
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 20:46:12 +0300

    фикс анимации

--- src/app/components/Day/Day.js
+++ src/app/components/Day/Day.js
@@ -1,12 +1,13 @@
 'use client';
 
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Subject from '../Subject/Subject';
 import styles from "./day.module.scss";
 
-export default function Day({day, subjects, isCurrentDay, showAllDays}) {
+export default function Day({ day, subjects, isCurrentDay, showAllDays }) {
     const [isOpen, setIsOpen] = useState(false);
     const contentRef = useRef(null);
+    const contentLineRef = useRef(null);
 
     useEffect(() => {
         if (showAllDays || isCurrentDay) {
@@ -19,8 +20,16 @@ export default function Day({day, subjects, isCurrentDay, showAllDays}) {
     useEffect(() => {
         if (isOpen && contentRef.current) {
             contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+            contentRef.current.style.padding = '10px 20px 20px';
+            if (contentLineRef.current) {
+                contentLineRef.current.style.opacity = '1';
+            }
         } else if (contentRef.current) {
             contentRef.current.style.maxHeight = '0';
+            contentRef.current.style.padding = '0';
+            if (contentLineRef.current) {
+                contentLineRef.current.style.opacity = '0';
+            }
         }
     }, [isOpen]);
 
@@ -57,16 +66,14 @@ export default function Day({day, subjects, isCurrentDay, showAllDays}) {
         <div className={`${styles.day} ${isCurrentDay ? styles.day_current : ''}`}>
             <div className={styles.day__panel} onClick={toggleOpen}>
                 <span className={styles.day__title}>{dayName}</span>
-
                 <img
                     src={'/svg/arrow.svg'}
                     alt={'arrow'}
                     className={styles.day__arrow}
-                    style={{transform: isOpen ? 'rotate(0deg)' : 'rotate(-90deg)'}}
+                    style={{ transform: isOpen ? 'rotate(0deg)' : 'rotate(-90deg)' }}
                 />
             </div>
-
-            <div className={styles.day__contentLine}></div>
+            <div className={styles.day__contentLine} ref={contentLineRef}></div>
             <div className={styles.day__content} ref={contentRef}>
                 {subjects.length > 0 ? (
                     subjects.map((item, idx) => (
@@ -77,7 +84,8 @@ export default function Day({day, subjects, isCurrentDay, showAllDays}) {
                             teacher={item.teacher}
                             isToday={isCurrentDay}
                         />
-                    ))) : (
+                    ))
+                ) : (
                     <span>Отдыхаем сегодня</span>
                 )}
             </div>
