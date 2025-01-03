0daa55752bc2b6d8396bdcb3cad9b91f724bc639
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 19:40:20 +0300

    автоскролл по дате

--- src/app/components/DatePicker/DatePicker.js
+++ src/app/components/DatePicker/DatePicker.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './datePicker.module.scss';
 import { ru } from 'date-fns/locale';
 
 const CustomDatePicker = ({ selectedDate, onChange }) => {
+    const datePickerRef = useRef(null);
+
+    useEffect(() => {
+        if (datePickerRef.current) {
+            datePickerRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            setTimeout(() => {
+                window.scrollBy(0, -80);
+            }, 1);
+        }
+    }, [selectedDate]);
+
     return (
-        <DatePicker
-            selected={selectedDate}
-            onChange={onChange}
-            dateFormat="dd/MM/yyyy"
-            locale={ru}
-            weekStartsOn={1}
-            className={styles.datePicker}
-        />
+        <div ref={datePickerRef}>
+            <DatePicker
+                selected={selectedDate}
+                onChange={onChange}
+                dateFormat="dd/MM/yyyy"
+                locale={ru}
+                weekStartsOn={1}
+                className={styles.datePicker}
+            />
+        </div>
     );
 };
 
