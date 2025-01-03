c2ec2889d7251897baf12395e704ddc7b103894d
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 14:12:25 +0300

    хедер обновил

--- src/app/layouts/header/header.js
+++ src/app/layouts/header/header.js
@@ -1,10 +1,11 @@
 import styles from "./header.module.scss";
 import stylesLayout from "../../layout.module.scss";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header>
-            <div className={stylesLayout.container}>
+            <div className={`${stylesLayout.container} ${stylesLayout.container_header}`}>
                 <div className={styles.header}>
                     <img
                         className={styles.header__logo}
@@ -13,6 +14,12 @@ export default function Header() {
                     />
 
                     <h1 className={styles.header__title}>Retab</h1>
+
+                    <div className={styles.header__links}>
+                        <Link href={'/profile'}>
+                            <span className={styles.header__link}>Личный кабинет</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </header>
