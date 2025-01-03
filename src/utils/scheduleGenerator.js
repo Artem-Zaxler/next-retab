c0ff2bd9400916d7877a93a6260202d8abe21e1e
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 18:43:40 +0300

    стилизовал

--- src/utils/scheduleGenerator.js
+++ src/utils/scheduleGenerator.js
@@ -16,12 +16,12 @@ export function generateSchedule(startOfWeek) {
 
 function generateDaySchedule(date) {
     const subjects = [
-        { subject: 'Математика', cabinet: 'В-113', teacher: 'Иванов И. И.' },
-        { subject: 'Физика', cabinet: 'Г-214', teacher: 'Петров П. П.' },
-        { subject: 'Химия', cabinet: 'Д-315', teacher: 'Сидоров С. С.' },
-        { subject: 'Биология', cabinet: 'А-416', teacher: 'Кузнецов К. К.' },
-        { subject: 'История', cabinet: 'Б-517', teacher: 'Смирнов С. С.' },
-        { subject: 'Искусство', cabinet: 'В-618', teacher: 'Попов П. П.' },
+        { subject: 'Java-технологии', cabinet: 'В-113', teacher: 'Иванов И. И.' },
+        { subject: 'Программные методы обработки изображений и распознавания образов', cabinet: 'Г-214', teacher: 'Петров П. П.' },
+        { subject: 'Программное обеспечение микропроцессоров и микроконтроллеров', cabinet: 'Д-315', teacher: 'Сидоров С. С.' },
+        { subject: 'Технологии искусственного интеллекта', cabinet: 'А-416', teacher: 'Кузнецов К. К.' },
+        { subject: 'Проектный практикум по управлению разработкой и разработке программного обеспечения', cabinet: 'Б-517', teacher: 'Смирнов С. С.' },
+        { subject: 'Цифровые системы автоматизированного управления', cabinet: 'В-618', teacher: 'Попов П. П.' },
     ];
 
     const daySchedule = [];
