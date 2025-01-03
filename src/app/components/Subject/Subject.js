e802c987486d47aff7c1702d42bc1c6e5af79282
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 19:02:02 +0300

    стили

--- src/app/components/Subject/Subject.js
+++ src/app/components/Subject/Subject.js
@@ -7,9 +7,9 @@ export default function Subject ({ subject, cabinet, teacher, isToday }) {
             <span className={styles.subject__title}>{subject}</span>
 
             <div className={styles.subject__bottom}>
-                <span>{cabinet}</span>
+                <span className={styles.subject__cabinet}>{cabinet}</span>
 
-                <span>{teacher}</span>
+                <span className={styles.subject__teacher}>{teacher}</span>
             </div>
         </div>
     );
