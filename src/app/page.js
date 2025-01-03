ee9edfbe354eba828b6d492214404406efcfb55e
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 18:21:47 +0300

    еще чет с неделей

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import styles from './page.module.scss';
 import Day from "./components/Day/Day";
 import CustomDatePicker from './components/DatePicker/DatePicker';
-import { generateSchedule } from '../utils/scheduleGenerator';
+import {generateSchedule} from '../utils/scheduleGenerator';
 
 const backgrounds = [
     '/images/stage-backgrounds/groups-2.png',
@@ -14,10 +14,10 @@ const backgrounds = [
 ];
 
 const filters = [
-    { text: 'По группам', content: 'Расписание для групп' },
-    { text: 'По преподавателям', content: 'Расписание для преподавателей' },
-    { text: 'По кабинетам', content: 'Расписание по кабинетам' },
-    { text: 'По предмету', content: 'Расписание по предмету' },
+    {text: 'По группам', content: 'Расписание для групп'},
+    {text: 'По преподавателям', content: 'Расписание для преподавателей'},
+    {text: 'По кабинетам', content: 'Расписание по кабинетам'},
+    {text: 'По предмету', content: 'Расписание по предмету'},
 ];
 
 export default function Home() {
@@ -91,7 +91,7 @@ export default function Home() {
         startOfWeek.setDate(today.getDate() - dayOfWeek + 1); // Set to Monday
         const endOfWeek = new Date(startOfWeek);
         endOfWeek.setDate(startOfWeek.getDate() + 6); // Set to Sunday
-        setCurrentWeek({ start: startOfWeek, end: endOfWeek });
+        setCurrentWeek({start: startOfWeek, end: endOfWeek});
 
         console.log(`Current day: ${days[dayOfWeek]}`);
         console.log(`Current week: ${startOfWeek.toDateString()} to ${endOfWeek.toDateString()}`);
@@ -127,7 +127,7 @@ export default function Home() {
 
         if (currentWeek && (date < currentWeek.start || date > currentWeek.end)) {
             console.log(`Week changed from ${currentWeek.start.toDateString()} to ${currentWeek.end.toDateString()} to ${newStartOfWeek.toDateString()} to ${newEndOfWeek.toDateString()}`);
-            setCurrentWeek({ start: newStartOfWeek, end: newEndOfWeek });
+            setCurrentWeek({start: newStartOfWeek, end: newEndOfWeek});
             setScheduleData(generateSchedule(date));
         } else {
             console.log(`Week remains the same: ${currentWeek.start.toDateString()} to ${currentWeek.end.toDateString()}`);
@@ -141,7 +141,7 @@ export default function Home() {
     };
 
     return (
-        <div className={styles.home} style={{ paddingTop: `${pagePaddingTop}px` }}>
+        <div className={styles.home} style={{paddingTop: `${pagePaddingTop}px`}}>
             <img
                 src={backgrounds[index]}
                 alt={'background-image'}
@@ -161,34 +161,43 @@ export default function Home() {
                     </button>
                 ))}
             </div>
+            {scheduleData &&
+                <>
+                    <CustomDatePicker selectedDate={selectedDate} onChange={handleDateChange}/>
 
-            <CustomDatePicker selectedDate={selectedDate} onChange={handleDateChange} />
-
-            <div className={styles.home__showAllDays}>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={showAllDays}
-                        onChange={handleShowAllDaysChange}
-                    />
-                    Показывать всю неделю
-                </label>
-            </div>
+                    <div className={styles.home__showAllDays}>
+                        <input
+                            type="checkbox"
+                            checked={showAllDays}
+                            onChange={handleShowAllDaysChange}
+                        />
+                        <span>
+                            Показывать всю неделю
+                        </span>
+                    </div>
+                </>
+            }
 
             <div className={`${styles.home__content} ${scheduleData ? styles.home__content_hasSchedule : ""}`}>
                 {scheduleData ? (
                     <div className={styles.home__columns}>
                         <div className={styles.home__column}>
-                            <Day day="Monday" subjects={scheduleData.Monday} isCurrentDay={currentDay === 'Monday'} showAllDays={showAllDays} />
-                            <Day day="Thursday" subjects={scheduleData.Thursday} isCurrentDay={currentDay === 'Thursday'} showAllDays={showAllDays} />
+                            <Day day="Monday" subjects={scheduleData.Monday} isCurrentDay={currentDay === 'Monday'}
+                                 showAllDays={showAllDays}/>
+                            <Day day="Thursday" subjects={scheduleData.Thursday}
+                                 isCurrentDay={currentDay === 'Thursday'} showAllDays={showAllDays}/>
                         </div>
                         <div className={styles.home__column}>
-                            <Day day="Tuesday" subjects={scheduleData.Tuesday} isCurrentDay={currentDay === 'Tuesday'} showAllDays={showAllDays} />
-                            <Day day="Friday" subjects={scheduleData.Friday} isCurrentDay={currentDay === 'Friday'} showAllDays={showAllDays} />
+                            <Day day="Tuesday" subjects={scheduleData.Tuesday} isCurrentDay={currentDay === 'Tuesday'}
+                                 showAllDays={showAllDays}/>
+                            <Day day="Friday" subjects={scheduleData.Friday} isCurrentDay={currentDay === 'Friday'}
+                                 showAllDays={showAllDays}/>
                         </div>
                         <div className={styles.home__column}>
-                            <Day day="Wednesday" subjects={scheduleData.Wednesday} isCurrentDay={currentDay === 'Wednesday'} showAllDays={showAllDays} />
-                            <Day day="Saturday" subjects={scheduleData.Saturday} isCurrentDay={currentDay === 'Saturday'} showAllDays={showAllDays} />
+                            <Day day="Wednesday" subjects={scheduleData.Wednesday}
+                                 isCurrentDay={currentDay === 'Wednesday'} showAllDays={showAllDays}/>
+                            <Day day="Saturday" subjects={scheduleData.Saturday}
+                                 isCurrentDay={currentDay === 'Saturday'} showAllDays={showAllDays}/>
                         </div>
                     </div>
                 ) : (
