885eabf9c8e6c795f3ee4d44181ef66fe5e92865
zaxler <mr.bocharoff2015@yandex.ru>
2025-01-03 19:13:36 +0300

    footer

--- src/app/layouts/footer/footer.js
+++ src/app/layouts/footer/footer.js
@@ -4,9 +4,9 @@ import stylesLayout from "../../layout.module.scss";
 export default function Footer() {
     return (
         <footer>
-            <div className={stylesLayout.container}>
+            <div className={`${stylesLayout.container} ${stylesLayout.container_footer}`}>
                 <div className={styles.footer}>
-                    <h1 className={styles.footer__title}>Made by Retab Team</h1>
+                    <span className={styles.footer__title}>Made by Retab Team</span>
                 </div>
             </div>
         </footer>
